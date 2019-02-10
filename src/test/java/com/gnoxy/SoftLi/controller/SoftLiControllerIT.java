39b73b838d98d4603f9efdc670770b85a7f14e18
Patrick Maher <dev@gnoxy.com>
2019-02-10 11:09:00 -0600

    Changed driver to: com.mysql.jdbc.Driver

--- src/test/java/com/gnoxy/SoftLi/controller/SoftLiControllerIT.java
+++ src/test/java/com/gnoxy/SoftLi/controller/SoftLiControllerIT.java
@@ -29,12 +29,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
